03d749eb708b9a21b9f75c08714a307fd3fd4af7
Rani <ranisans@yahoo.com>
2020-02-09 14:29:51 +0300

    feat: move info texts to its own file

--- src/container/LoanBlock.jsx
+++ src/container/LoanBlock.jsx
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import InputBlock from './InputBlock';
 import ButtonBlock from './ButtonBlock';
 
+import { tradeInInfo, downPaymentInfo, APRInfo } from '../infoText';
+
 const StyledLoad = styled.div`
   display: grid;
   margin-top: 10px;
@@ -20,58 +22,46 @@ const LoanBlock = ({
   defaultCreditScore,
   zipCode,
   callbacks,
-}) => {
-  const tradeInInfo = `
-    The value of your currently owned vehicle credited towards the purchase or lease of the vehicle you
-    are acquiring.`;
-
-  const downPaymentInfo = 'The cash down payment will reduce the financed loan amount.';
-
-  const APRInfo = `
-    APR stands for annual percentage rate, which refers to the interest you’re being charged to borrow money.
-    APRs can be calculated as simple or compound interest, and rates can be fixed or variable.`;
-
-  return (
-    <StyledLoad>
-      <InputBlock
-        text="Location"
-        infoText="Your ZIP code."
-        initialValue={zipCode}
-        sign="ZIP"
-        callback={callbacks.zipCallback}
-      />
-      <ButtonBlock
-        buttons={termValues}
-        defaultButton={defaultTermValue}
-        callback={callbacks.termCallback}
-      />
-      <InputBlock
-        text="Trade-In"
-        infoText={tradeInInfo}
-        sign="$"
-        callback={callbacks.tradeInCallback}
-      />
-      <InputBlock
-        text="Down Payment"
-        infoText={downPaymentInfo}
-        sign="$"
-        callback={callbacks.downPaymentCallback}
-      />
-      <InputBlock
-        text="APR"
-        infoText={APRInfo}
-        sign="%"
-        callback={callbacks.APRCallback}
-        defaultButton={defaultTermValue}
-      />
-      <ButtonBlock
-        buttons={creditScoreValues}
-        defaultButton={defaultCreditScore}
-        callback={callbacks.creditScoreCallback}
-      />
-    </StyledLoad>
-  );
-};
+}) => (
+  <StyledLoad>
+    <InputBlock
+      text="Location"
+      infoText="Your ZIP code."
+      initialValue={zipCode}
+      sign="ZIP"
+      callback={callbacks.zipCallback}
+    />
+    <ButtonBlock
+      buttons={termValues}
+      defaultButton={defaultTermValue}
+      callback={callbacks.termCallback}
+    />
+    <InputBlock
+      text="Trade-In"
+      infoText={tradeInInfo}
+      sign="$"
+      callback={callbacks.tradeInCallback}
+    />
+    <InputBlock
+      text="Down Payment"
+      infoText={downPaymentInfo}
+      sign="$"
+      callback={callbacks.downPaymentCallback}
+    />
+    <InputBlock
+      text="APR"
+      infoText={APRInfo}
+      sign="%"
+      callback={callbacks.APRCallback}
+      defaultButton={defaultTermValue}
+    />
+    <ButtonBlock
+      buttons={creditScoreValues}
+      defaultButton={defaultCreditScore}
+      callback={callbacks.creditScoreCallback}
+    />
+  </StyledLoad>
+);
 
 LoanBlock.propTypes = {
   termValues: PropTypes.arrayOf(PropTypes.shape({
---
+++ src/infoText.js
@@ -0,0 +1,11 @@
+const tradeInInfo = `
+    The value of your currently owned vehicle credited towards the purchase or lease of the vehicle you
+    are acquiring.`;
+
+const downPaymentInfo = 'The cash down payment will reduce the financed loan amount.';
+
+const APRInfo = `
+    APR stands for annual percentage rate, which refers to the interest you’re being charged to borrow money.
+    APRs can be calculated as simple or compound interest, and rates can be fixed or variable.`;
+
+export { tradeInInfo, downPaymentInfo, APRInfo };
