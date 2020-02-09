96ad4bac9c41f1c3c64c5b634f7b9c44987cc357
Rani <ranisans@yahoo.com>
2020-02-09 16:39:14 +0300

    refact: change style to Input component

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
 const StyledInput = styled.div`
-  width: 300px;
+  width: 250px;
   height: 30px;
   display: flex;
   align-items: flex-end;
