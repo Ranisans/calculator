0208f0e8000fcf26ff8c2f523209dca66c553219
Rani <ranisans@yahoo.com>
2020-02-09 11:25:49 +0300

    feat: add Paragraph component

---
+++ src/component/Paragraph.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const StyledParagraph = styled.div`
+  width: 300px;
+  height: 30px;
+  margin: 0;
+
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+
+  font-size: 1.1rem;
+`;
+
+const Paragraph = ({ sign = '', value }) => (
+  <StyledParagraph>
+    {`${value}${sign.length > 0 ? ' ' : ''}${sign}`}
+  </StyledParagraph>
+);
+
+Paragraph.propTypes = {
+  sign: PropTypes.string,
+  value: PropTypes.string.isRequired,
+};
+
+Paragraph.defaultProps = {
+  sign: '',
+};
+
+export default Paragraph;
