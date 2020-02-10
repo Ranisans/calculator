76f0dbe4027050c98cb8d51f059bb8a99477f360
Ranisans <ranisans@yahoo.com>
2020-02-10 17:11:17 +0300

    feat: add LoanLeaseBlock

---
+++ src/container/LoanLeaseBlock.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import LoanBlock from './LoanBlock';
+import LeaseBlock from './LeaseBlock';
+import { LoanPosition } from '../constants';
+
+
+const LoanLeaseBlock = (props) => {
+  const termsState = useSelector((state) => state.terms);
+
+  const { props: val } = props;
+  const {
+    leaseTerms,
+    loanTerms,
+    creditScoreValues,
+    mileages,
+  } = val;
+
+  if (termsState.calculatorType === LoanPosition) {
+    return (
+      <LoanBlock
+        loanTermValues={loanTerms}
+        creditScoreValues={creditScoreValues}
+      />
+    );
+  }
+  return (
+    <LeaseBlock
+      leaseTermValues={leaseTerms}
+      mileagesValues={mileages}
+      creditScoreValues={creditScoreValues}
+    />
+  );
+};
+
+export default LoanLeaseBlock;
