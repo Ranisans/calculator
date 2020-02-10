5c74c55499dc459475b3441b7f3ffa220cb07d5f
Ranisans <ranisans@yahoo.com>
2020-02-10 10:04:38 +0300

    feat: add index file to mock data

---
+++ src/testData/index.js
@@ -0,0 +1,8 @@
+import creditScoreData from './creditScoreData';
+import infoCardMockData from './infoCardMockData';
+import loanDefaultData from './loanDefaultData';
+import leaseDefaultData from './leaseDefaultData';
+
+export {
+  creditScoreData, infoCardMockData, loanDefaultData, leaseDefaultData,
+};
