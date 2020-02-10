d748ce4d74848c492b4d6b193e9a1eaf257eb202
Ranisans <ranisans@yahoo.com>
2020-02-10 10:01:05 +0300

    feat: change size of Input component

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
 const StyledInput = styled.div`
-  width: 250px;
+  width: 150px;
   height: 30px;
   display: flex;
   align-items: flex-end;
