2807a8aa7e54002e19007d1dd321282bcb2ef838
Ranisans <ranisans@yahoo.com>
2020-02-10 17:10:17 +0300

    feat: add redux state to LoanBlock

--- src/container/LoanBlock.jsx
+++ src/container/LoanBlock.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputBlock from './InputBlock';
 import ButtonBlock from './ButtonBlock';
 
 import { tradeInInfo, downPaymentInfo, APRInfo } from '../infoText';
+import { changeZip, changeScore } from '../actions/paramsAction';
+import { changeLoanTerm } from '../actions/termsAction';
+import { changeTradeIn, changeDownPayment, changeAPR } from '../actions/variablesAction';
 
 const StyledLoad = styled.div`
   display: grid;
@@ -16,73 +20,86 @@ const StyledLoad = styled.div`
 `;
 
 const LoanBlock = ({
-  termValues,
-  defaultTermValue,
+  loanTermValues,
   creditScoreValues,
-  defaultCreditScore,
-  zipCode,
-  callbacks,
-}) => (
-  <StyledLoad>
-    <InputBlock
-      text="Location"
-      infoText="Your ZIP code."
-      initialValue={zipCode}
-      sign="ZIP"
-      callback={callbacks.zipCallback}
-    />
-    <ButtonBlock
-      buttons={termValues}
-      defaultButton={defaultTermValue}
-      callback={callbacks.termCallback}
-    />
-    <InputBlock
-      text="Trade-In"
-      infoText={tradeInInfo}
-      sign="$"
-      callback={callbacks.tradeInCallback}
-    />
-    <InputBlock
-      text="Down Payment"
-      infoText={downPaymentInfo}
-      sign="$"
-      callback={callbacks.downPaymentCallback}
-    />
-    <InputBlock
-      text="APR"
-      infoText={APRInfo}
-      sign="%"
-      callback={callbacks.APRCallback}
-      defaultButton={defaultTermValue}
-    />
-    <ButtonBlock
-      buttons={creditScoreValues}
-      defaultButton={defaultCreditScore}
-      callback={callbacks.creditScoreCallback}
-    />
-  </StyledLoad>
-);
+}) => {
+  const dispatch = useDispatch();
+  const variablesState = useSelector((state) => state.variables);
+  const termsState = useSelector((state) => state.terms);
+  const paramsState = useSelector((state) => state.params);
+
+  const zipHandler = (newZip) => {
+    dispatch(changeZip({ value: newZip }));
+  };
+  const loanTermHandler = (leaseTermValue) => {
+    dispatch(changeLoanTerm({ term: parseInt(leaseTermValue, 10) }));
+  };
+  const tradeInHandler = (tradeInValue) => {
+    dispatch(changeTradeIn({ value: tradeInValue }));
+  };
+  const downPaymentHandler = (downPaymentValue) => {
+    dispatch(changeDownPayment({ value: downPaymentValue }));
+  };
+  const aprHandler = (aprValue) => {
+    dispatch(changeAPR({ value: aprValue }));
+  };
+  const creditScoreHandler = (creditScoreNumber) => {
+    dispatch(changeScore({ score: parseInt(creditScoreNumber, 10) }));
+  };
+
+  console.log('TCL: termsState.loanTerm', termsState);
+  return (
+    <StyledLoad>
+      <InputBlock
+        text="Location"
+        infoText="Your ZIP code."
+        initialValue={paramsState.zipCode}
+        sign="ZIP"
+        callback={zipHandler}
+      />
+      <ButtonBlock
+        buttons={loanTermValues}
+        defaultButton={termsState.loanTerm}
+        callback={loanTermHandler}
+      />
+      <InputBlock
+        text="Trade-In"
+        infoText={tradeInInfo}
+        sign="$"
+        initialValue={variablesState.tradeIn}
+        callback={tradeInHandler}
+      />
+      <InputBlock
+        text="Down Payment"
+        infoText={downPaymentInfo}
+        sign="$"
+        initialValue={variablesState.downPayment}
+        callback={downPaymentHandler}
+      />
+      <InputBlock
+        text="APR"
+        infoText={APRInfo}
+        sign="%"
+        callback={aprHandler}
+      />
+      <ButtonBlock
+        buttons={creditScoreValues}
+        defaultButton={paramsState.score}
+        callback={creditScoreHandler}
+      />
+    </StyledLoad>
+  );
+};
 
 LoanBlock.propTypes = {
-  termValues: PropTypes.arrayOf(PropTypes.shape({
+  loanTermValues: PropTypes.arrayOf(PropTypes.shape({
     text: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
   })).isRequired,
-  defaultTermValue: PropTypes.number.isRequired,
   creditScoreValues: PropTypes.arrayOf(PropTypes.shape({
     text: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
   })).isRequired,
-  defaultCreditScore: PropTypes.number.isRequired,
-  zipCode: PropTypes.number.isRequired,
-  callbacks: PropTypes.shape({
-    zipCallback: PropTypes.func.isRequired,
-    termCallback: PropTypes.func.isRequired,
-    tradeInCallback: PropTypes.func.isRequired,
-    downPaymentCallback: PropTypes.func.isRequired,
-    APRCallback: PropTypes.func.isRequired,
-    creditScoreCallback: PropTypes.func.isRequired,
-  }).isRequired,
 };
 
 export default LoanBlock;
