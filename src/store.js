c6c228e178e5000d9e6ab1e9ba6bd2eb6059b51c
Rani <ranisans@yahoo.com>
2020-02-12 20:39:21 +0300

    fix: remove console.log

--- src/store.js
+++ src/store.js
@@ -4,7 +4,6 @@ import rootReducer from './reducers/rootReducer';
 import { loadStorage, saveState } from './logic/localStorage';
 
 const persistedState = loadStorage();
-console.log('TCL: persistedState', persistedState);
 
 const store = createStore(
   rootReducer,
